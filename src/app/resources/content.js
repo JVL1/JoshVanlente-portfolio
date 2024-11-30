e74abf6c70027607908f47db7ec1f619661affcd
JVL1 <jvanlente.e3@gmail.com>
2024-11-29 21:41:01 -0800

    Update content and components for portfolio site

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -7,9 +7,9 @@ const person = {
         return `${this.firstName} ${this.lastName}`;
     },
     role:      'Product Leader',
-    avatar:    '/images/avatar.jpg',
+    avatar:    '/images/avatar.jpeg',
     location:  'America/Los Angeles',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-    languages: ['English']  // optional: Leave the array empty if you don't want to display languages
+    languages: ['English'],
 }
 
 const newsletter = {
@@ -24,12 +24,12 @@ const social = [
     {
         name: 'GitHub',
         icon: 'github',
-        link: 'https://github.com/once-ui-system/nextjs-starter',
+        link: 'https://github.com/JVL1',
     },
     {
         name: 'LinkedIn',
         icon: 'linkedin',
-        link: 'https://www.linkedin.com/company/once-ui/',
+        link: 'https://www.linkedin.com/in/josh-van-lente/',
     },
     {
         name: 'X',
@@ -39,7 +39,7 @@ const social = [
     {
         name: 'Email',
         icon: 'email',
-        link: 'mailto:example@gmail.com',
+        link: 'mailto:Josh@vanlente.net',
     },
 ]
 
@@ -47,8 +47,8 @@ const home = {
     label: 'Home',
     title: `${person.name}'s Portfolio`,
     description: `Portfolio website showcasing my work as a ${person.role}`,
-    headline: <>Design engineer and builder</>,
-    subline: <>I'm Selene, a design engineer at <InlineCode>FLY</InlineCode>, where I craft intuitive<br/> user experiences. After hours, I build my own projects.</>
+    headline: <>Product Leader and builder</>,
+    subline: <>I'm Josh, a product leader at <InlineCode>Azibo</InlineCode>, where I am building the best all-in-one platform for rental property owners.<br/> After hours, I build my own projects.</>
 }
 
 const about = {
@@ -61,27 +61,28 @@ const about = {
     },
     avatar: {
         display: true
-    },
-    calendar: {
-        display: true,
-        link: 'https://cal.com'
+    },calendar: {
+        display: false,
+        link: ''
     },
     intro: {
         display: true,
         title: 'Introduction',
-        description: <>Selene is a Jakarta-based design engineer with a passion for transforming complex challenges into simple, elegant design solutions. Her work spans digital interfaces, interactive experiences, and the convergence of design and technology.</>
+        description: <>Josh is an experienced product leader with a strong track record of delivering impactful results as both a team leader and hands-on contributor. Highly adaptable and skilled at navigating the ambiguity of 0-to-1 innovation, as well as, the complexity of scaling established products. With expertise spanning fintech, proptech, adtech, and both B2C and B2B markets, I bring strategic vision, cross-functional collaboration, and a passion for continuous learning to every challenge.
+</>
     },
     work: {
         display: true, // set to false to hide this section
         title: 'Work Experience',
         experiences: [
             {
-                company: 'FLY',
-                timeframe: '2022 - Present',
-                role: 'Senior Design Engineer',
+                company: 'Azibo',
+                timeframe: '2023 - Present',
+                role: 'Senior Manager, Product Management',
                 achievements: [
-                    <>Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user engagement and 30% faster load times.</>,
-                    <>Spearheaded the integration of AI tools into design workflows, enabling designers to iterate 50% faster.</>
+                    <>Led as a player-coach, a team of PMs and QA and directly drove a 2.8x increase in monetized users and a 3.5x growth in gross margin per user over 2 years at Azibo.</>,
+                    <>Developed and executed a vision to expand the product suite from rent collection to a comprehensive offering of 7 products, including accounting, banking, and lease creation.</>,
+                    <>Redesigned the payment platform to minimize support-requiring edge cases and add new growth-enabling features; now processes over $300 million in payments annually.</>
                 ],
                 images: [ // optional: leave the array empty if you don't want to display images
                     {
@@ -93,14 +94,38 @@ const about = {
                 ]
             },
             {
-                company: 'Creativ3',
-                timeframe: '2018 - 2022',
-                role: 'Lead Designer',
+                company: 'Azibo',
+                timeframe: '2022 - 2023',
+                role: 'Senior Product Manager',
+                achievements: [
+                    <>Activated what is now the primary growth channel and most profitable user segment through a unique product led growth initiative; including closing multiple partnerships (<a href="https://www.azibo.com/blog/azibo-partners-with-rentec-north-america">1</a>).</>,
+                    <>Led the effort to rebuild the entire UX which now frequently results in kudos from customers and prospects, as well as, won several industry awards (<a href="https://proptechbreakthrough.com/2023-winners/">1</a>, <a href="https://gdusa.com/2024-digital-design-awards-winner?rns=0|4824">2</a>).</>
+                ],
+                images: [ // optional: leave the array empty if you don't want to display images
+                    {
+                        src: '/images/projects/project-01/rhawa.jpg',
+                        alt: 'RHAWA partnership',
+                        width: 16,
+                        height: 9
+                    }
+                ]
+            },
+            {
+                company: 'Upstart',
+                timeframe: '2019 - 2022',
+                role: 'Prouct Manager',
                 achievements: [
                     <>Developed a design system that unified the brand across multiple platforms, improving design consistency by 40%.</>,
                     <>Led a cross-functional team to launch a new product line, contributing to a 15% increase in overall company revenue.</>
                 ],
-                images: [ ]
+                images: [
+                    {
+                    src: '/images/projects/project-01/cover-01.jpg',
+                    alt: 'Once UI Project',
+                    width: 16,
+                    height: 9
+                    }
+                ]
             }
         ]
     },
