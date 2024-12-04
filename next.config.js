888626b91fcaf103bf989bb166cfae8211800eea
JVL1 <jvanlente.e3@gmail.com>
2024-12-03 22:34:04 -0800

    fix: update webpack config for css, scss, and svg

--- next.config.js
+++ next.config.js
@@ -13,10 +13,34 @@ const nextConfig = {
         punycode: false,
       };
     }
+
+    // Handle CSS and SCSS
     config.module.rules.push({
       test: /\.(css|scss)$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+          },
+        },
+        'sass-loader',
+      ],
     });
+
+    // Handle SVG files
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    // Handle Remixicon CSS
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'remixicon/fonts/remixicon.css': require.resolve('remixicon/fonts/remixicon.css'),
+    };
+
     return config;
   },
 };
