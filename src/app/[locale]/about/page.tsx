2755e3735b83053ac2a257fe391e82da6a930469
JVL1 <jvanlente.e3@gmail.com>
2024-12-04 20:35:32 -0800

    fix: restore Once UI component imports

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,14 +1,11 @@
 import { Avatar, Button, Flex, Heading, Icon, IconButton, SmartImage, Tag, Text } from '@/once-ui/components';
 import { baseURL, renderContent } from '@/app/resources';
 import TableOfContents from '@/components/about/TableOfContents';
-import { CaseStudyPreview } from '@/components/about/CaseStudyPreview';
+import { CaseStudyPreview, SkillCard, WorkCard, EducationCard } from '@/components';
 import styles from '@/components/about/about.module.scss'
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import { useTranslations } from 'next-intl';
 import { getPosts } from '@/app/utils/utils';
-import { SkillCard } from '@/components/about/SkillCard';
-import { WorkCard } from '@/components/about/WorkCard';
-import { EducationCard } from '@/components/about/EducationCard';
 
 export async function generateMetadata(
     {params: {locale}}: { params: { locale: string }}
