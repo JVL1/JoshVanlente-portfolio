dd78c89fba5f5150eabc014e8beaf43e7dd8e607
JVL1 <jvanlente.e3@gmail.com>
2024-12-04 20:21:25 -0800

    fix: add about component exports to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,9 @@ export { Mailchimp } from '@/components/Mailchimp'
 export { ProjectCard } from '@/components/ProjectCard'
 export { HeadingLink } from '@/components/HeadingLink'
 export { RouteGuard } from '@/components/RouteGuard'
+
+// About components
+export { CaseStudyPreview } from '@/components/about/CaseStudyPreview'
+export { SkillCard } from '@/components/about/SkillCard'
+export { WorkCard } from '@/components/about/WorkCard'
+export { EducationCard } from '@/components/about/EducationCard'
